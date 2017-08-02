eb597ab37b34b26923e1907b0f6922b476f75f0b
Jakob <jdg7sh@virginia.edu>
2017-08-02 15:47:00 -0400

    Lots of work on bringing in pokemon json data and creating a way to search for pokemon and moves

--- app/init.php
+++ app/init.php
@@ -2,10 +2,20 @@
 
 require_once 'vendor/autoload.php';
 
-$hosts = [
-    'https://localhost',        // SSL to localhost
-];
+require 'vendor/autoload.php';
 
-$client = Elasticsearch\ClientBuilder::create()           // Instantiate a new ClientBuilder
-                    ->setHosts($hosts)      // Set the hosts
-                    ->build();              // Build the client object
+use Elasticsearch\ClientBuilder;
+
+$es = ClientBuilder::create()->build();
+
+//$hosts = [
+//    'https://localhost',        // SSL to localhost
+//];
+
+//$es = new Elasticsearch\Client([
+//    'hosts' => ['127.0.0.1:9200']
+//]);
+
+//$client = Elasticsearch\ClientBuilder::create()           // Instantiate a new ClientBuilder
+//                    ->setHosts($hosts)      // Set the hosts
+//                    ->build();              // Build the client object
--- index.php
+++ index.php
@@ -1,13 +1,127 @@
 <!DOCTYPE html>
-<?php require_once 'app/init.php'; ?>
+<?php
+require_once 'app/init.php';
+
+if (!empty($_POST)) {
+
+    if (isset($_POST['title'], $_POST['body'], $_POST['keywords'])) {
+
+        $title = $_POST['title'];
+        $body = $_POST['body'];
+        $keywords = explode(',', $_POST['keywords']);
+
+        $indexed = $es->index([
+            'index' => 'articles',
+            'type' => 'article',
+            'body' => [
+                'title' => $title,
+                'body' => $body,
+                'keywords' => $keywords
+            ]
+        ]);
+
+        if ($indexed) {
+            print_r($indexed);
+        }
+    }
+}
+
+if (isset($_GET['q'])) {
+    $q = $_GET['q'];
+
+//    $params = [
+//        'index' => 'pokemon',
+//        'type' => 'moves',
+//        'id' => $q
+//    ];
+//
+//// Get doc at /my_index/my_type/my_id
+//    $query = $es->get($params);
+//    
+//    echo '<pre>' . json_encode($query, JSON_PRETTY_PRINT) . '</pre>';
+//    
+//    if (sizeof($query['_source']) >= 1) {
+//        $results = $query['_source'];
+//    }
+
+    $query = $es->search([
+        'size' => 50,
+        'body' => [
+            'query' => [
+                'bool' => [
+                    'should' => [
+                        ['match' => ['name' => $q]],
+                        ['match' => ['power' => $q]]
+                    ]
+                ]
+            ]
+        ]
+    ]);
+
+//    echo '<pre>' . print_r($query) . '</pre>';
+
+    if ($query['hits']['total'] >= 1) {
+        $results = $query['hits']['hits'];
+    }
+}
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
     </head>
     <body>
-        <?php
-        echo "Hello friends";
+        <!--        <form class="form-group" action="." method="post">
+                    <label>
+                        Title
+                        <input type="text" name="title">
+                    </label>
+                    <label>
+                        Body
+                        <textarea rows="5" name="body"></textarea>
+                    </label>
+                    <label>
+                        Keywords
+                        <input type="text" name="keywords">
+                    </label>
+                    <input type="submit" name="submit">
+                </form>-->
+
+        <form action="." method="get">
+            <label>
+                Search for something
+                <input type="text" name="q">
+            </label>
+
+            <input type="submit" value="Search">
+        </form>
+
+<?php
+if (isset($results)) {
+    foreach ($results as $r) {
         ?>
+                <div class="result">
+                <?php
+                echo 'Title: ' . $r['_source']['name'] . '<br>';
+                echo 'Power: ' . $r['_source']['power'] . '<br>';
+                echo 'PP: ' . $r['_source']['pp'] . '<br>';
+                echo 'Accuracy: ' . $r['_source']['accuracy'];
+                ?>
+                <?php
+//                echo 'Title: ' . $results['name'] . '<br>';
+//                echo 'Power: ' . $results['power'] . '<br>';
+//                echo 'PP: ' . $results['pp'] . '<br>';
+//                echo 'Accuracy: ' . $results['accuracy'];
+                ?>
+                </div>
+                <br>
+
+
+        <?php
+    }
+}
+?>
+
+
     </body>
 </html>
